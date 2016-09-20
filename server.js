54b32ae67106d2d6f5e00ee37afe14d792de8aff
sumigopi <gopi8953@gmail.com>
2016-09-20 16:55:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('C:\Users\Admin\Desktop\Penguins.jpg', function (req, res) {
+app.get('/ui/Penguins.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
