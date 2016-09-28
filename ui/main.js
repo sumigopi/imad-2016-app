0536c4ad6c3feb50ce43b674a1a3b6a630e728b4
sumigopi <gopi8953@gmail.com>
2016-09-28 17:02:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,5 +17,5 @@ function moveRight() {
     img.style.marginLeft = marginLeft + 'px';
     }
     img.onclick = function() {
-        var intervel = setIntervel(moveRight, 50);
+        var interval = setInterval(moveRight, 50);
     }
