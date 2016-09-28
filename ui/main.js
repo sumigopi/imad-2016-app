87062a693fc0bcd42584e6c55107912880acfb91
sumigopi <gopi8953@gmail.com>
2016-09-28 16:40:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,15 @@ element.innerHTML = "New Value"
 
 //Move the image
 var img = document.getElementById ("Praveen");
-img.onclick = function() {
+/*img.onclick = function() {
     img.style.marginLeft ="100px";
-}
+}*/
+
+var marginleft = 0;
+function moveRight() {
+    marginLeft = MarginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px'
+    }
+    img.onclick = function() {
+        var intervel = setIntervel(moveRight, 50)
+    }
