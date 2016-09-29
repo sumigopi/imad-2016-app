366161c4bea62462206a35873d2723962ab39769
sumigopi <gopi8953@gmail.com>
2016-09-29 17:10:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,6 +39,17 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names = [];
+app.get('/submit-name/name', function(req, res) {
+    //get the name from the request
+    var name - req.perarms.name;
+    
+    var.push(name);
+    //JSON Javascript Object Notation
+    
+    res.send(JSON.stringify(names));
+}
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
