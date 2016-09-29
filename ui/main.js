cb5602ca339d779015083c156ba6950e62d9a9c9
sumigopi <gopi8953@gmail.com>
2016-09-29 13:27:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 alert("Hi! I am here");
 //To change the exixting text
 var element = document.getElementById("main-text");
@@ -11,11 +11,30 @@ var img = document.getElementById ("Praveen");
     img.style.marginLeft ="100px";
 }*/
 
-var marginLeft = 0;
+/*var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
     }
     img.onclick = function() {
         var interval = setInterval(moveRight, 50);
-    }
+    }*/
+//counter code
+var button = document.getElementById("counter");
+var counter = 0
+button.onClick = function() {
+    
+   //Make request to counter end point 
+    
+    
+    //Capture the response and store it in a variable
+    
+    
+    
+    //render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+    
+    
+}
