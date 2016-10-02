d6738192212044863c4370e9fccdace22d90b147
sumigopi <gopi8953@gmail.com>
2016-10-02 17:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,12 +132,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.send('Article two requested and will be sent here');
-});
 
-app.get('/article-three', function (req, res) {
-  res.send('Article three requested and will be sent here');
 });
 
 app.get('/ui/style.css', function (req, res) {
