2b8b8c4740ecce2d5606e6367cba070d3e98f86a
sumigopi <gopi8953@gmail.com>
2016-10-06 15:07:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-/*console.log('Loaded!');
+console.log('Loaded!');
 alert("Hi! I am here");
 //To change the exixting text
 var element = document.getElementById("main-text");
@@ -7,18 +7,18 @@ element.innerHTML = "New Value"
 
 //Move the image
 var img = document.getElementById ("Praveen");
-/*img.onclick = function() {
+img.onclick = function() {
     img.style.marginLeft ="100px";
-}*/
+}
 
-/*var marginLeft = 0;
+var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
     }
     img.onclick = function() {
         var interval = setInterval(moveRight, 50);
-    }*/
+    }
 //counter code
 var button = document.getElementById("counter");
 
