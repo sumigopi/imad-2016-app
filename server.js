3a42c1cd8d37a843386609b576b40963297f9c21
sumigopi <gopi8953@gmail.com>
2016-10-07 19:05:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ app.get('/submit-name/name', function(req, res) {
     //JSON Javascript Object Notation
     
     res.send(JSON.stringify(names));
-}
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
