c8c10acf1f6389917f13dc0beaa9a3b538431f66
sumigopi <gopi8953@gmail.com>
2016-10-09 16:31:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,12 +139,6 @@ app.get('/:articleName', function (req, res) {
 });
 
 
-
-
-
-
-
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
