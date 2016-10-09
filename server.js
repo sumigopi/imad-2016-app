8c21c03f7434441990f87e96112afc6b4973cce8
sumigopi <gopi8953@gmail.com>
2016-10-09 14:48:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+ counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter+1;
+    res.send(counter.toString());
+});
+
+
+
 app.get('/:articleName', function (req, res) {
     //articleName = article-one etc
     //articles(articleName)= content object of article-one
