b9c9f6d7eeb2ec1486b140d9cc3e5a92859b9cb3
sumigopi <gopi8953@gmail.com>
2016-10-13 16:10:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,24 +51,36 @@ request.open("GET","http://sumigopi.imad.hasura-app.io/counter",true);
 request.send("null");
 };
   //Submit Name
-  var nameInput =document.getElementById("name");
-  var name = nameInput.value;
+ 
   var submit = document.getElementById("submit_btn");
   submit.onclick = function(){
-      //Make a request to the server and send the name
+     //Create request object
+   var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+   request.onreadystatechange = function() {
+       if (request.readyState === XMLHttpRequest.DONE){
+           //Take some action
+           if(request.status === 200) {
+   
       //Capture a list of names and render it as a list
-     var names = ["Name1","Name2","Name3","Name4"]; 
+     var names = request.responseText; 
+     names = JSON.parse(names);
      var list = " ";
      for(var i=0; i<names.length; i++) {
          list += "<li>" + names[i] + "</li>";
      }
-     
-  
       
      var ul = document.getElementById("namelist");
      ul.innerHTML = list;
-     
-      
-      
+           }
+       }
+   } 
+    
+        // Make the request
+ var nameInput =document.getElementById("name");
+  var name = nameInput.value;        
+request.open("GET","http://sumigopi.imad.hasura-app.io/submit-name?=" + name,true);
+request.send("null");  
   }; 
   
