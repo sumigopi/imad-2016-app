f5e4d6d9ae414ae96a8299a52a99b4b3c615db3b
sumigopi <gopi8953@gmail.com>
2016-11-01 15:39:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     host:db.imad.hasura-app.io,
     port:'5432',
     password: process.env.DB_PASSWORD
-    }
+    };
 
 
 var app = express();
@@ -22,7 +22,7 @@ app.get('/test-db', function (req, res) {
         if (err) {
             res.status(500).send(err.toString());
             } else {
-            res.send(JASON.toStringify(result.rows))
+            res.send(JASON.toStringify(result.rows));
             }
         });
 
