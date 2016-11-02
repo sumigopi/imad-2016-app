220af7e7bb2f7b0755f448c2ca43f45923ba760f
sumigopi <gopi8953@gmail.com>
2016-11-02 15:08:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: "sumigopi",
     host:"db.imad.hasura-app.io",
     port:"5432",
-    password: "process.env.DB_PASSWORD"
+    password: process.env.DB_PASSWORD
     };
 
 
